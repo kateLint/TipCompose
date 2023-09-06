7cc1638db6df94b519025f28ebf71510bb2d2e7a
katelint <ktlint@gmail.com>
2023-09-06 14:16:38 +0300

    new tip compose project beginning

--- app/src/main/java/com/compose/tipcomposeproj/components/Components.kt
+++ app/src/main/java/com/compose/tipcomposeproj/components/Components.kt
@@ -1,8 +1,9 @@
 package com.compose.tipcomposeproj.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun inputField(){
+fun inputField(modifier: Modifier = Modifier){
 
 }
